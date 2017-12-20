193ac9682ac18b3705e4cf8bf7f1ffe1b755235e
Beatriz de Miguel <beatrizdemiguelperez@gmail.com>
2017-12-20 18:22:45 +0100

    udpate node-test

--- node-tests/index.js
+++ node-tests/index.js
@@ -7,27 +7,46 @@ var exec = require('child_process').exec;
 var emberCLIPath = path.resolve(__dirname, './fixtures/simple-app/node_modules/ember-cli/bin/ember');
 var fixturePath = path.resolve(__dirname, './fixtures/simple-app');
 
-describe('Acceptance Tests', function () {
+describe('Addon is enabled for production build', function () {
 	this.timeout(120000);
 
-	context('A Simple App', function () {
+	context('Precaches and register serviceworker', () => {
 
+		before(() => runEmberCommand(fixturePath, 'build --prod'));
 
-		before(function () {
-			return runEmberCommand(fixturePath, 'build');
-		});
+		after(() => cleanup(fixturePath));
 
-		after(function () {
-			cleanup(fixturePath);
-		});
-
-		it('produces a sw.js file', function () {
+		it('produces a sw.js file', () => {
 			exists(outputFilePath('sw.js'));
 		});
 
-		it('produces a sw skip waiting file, which is imported on sw.js', function () {
+		it('precaches assets', () => {
+			contains(outputFilePath('sw.js'), /assets\/simple-app-\w\.js/);
+		});
+
+		it('produces a sw skip waiting file, which is imported on sw.js', () => {
 			exists(outputFilePath('assets/service-workers/skip-waiting.js'));
-		contains(outputFilePath('sw.js'), /skip-waiting\.js/);
+			contains(outputFilePath('sw.js'), /skip-waiting\.js/);
+		});
+	});
+});
+
+
+describe('Addon is disabled for development', function () {
+	this.timeout(120000);
+
+	context('Precaches nothing and register serviceworker', () => {
+
+		before(() => runEmberCommand(fixturePath, 'build'));
+
+		after(() => cleanup(fixturePath));
+
+		it('produces a sw.js file', () => {
+			exists(outputFilePath('sw.js'));
+		});
+
+		it('precaches nothing', () => {
+			contains(outputFilePath('sw.js'), /precacheManifest\s\=\s\[\]/);
 		});
 	});
 });
@@ -60,6 +79,9 @@ function exists(filePath) {
 
 function contains(filePath, regexp) {
 	var fileContent = fs.readFileSync(filePath, 'utf8');
+
+	console.log(fileContent);
+	console.log(fileContent.match(regexp));
 
 	assert.ok(fileContent.match(regexp), filePath + ' contains ' + regexp);
 }
